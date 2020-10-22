42342985f60187b2296c9723167f4b728c6e962e
Dinh Hoang Nhi <nhidh99@gmail.com>
2020-10-22 19:56:43 +0700

    #8 Fix Slider

--- laptop-store-webui-v2/src/scenes/Guest/scenes/HomePage/components/Slider/styles.ts
+++ laptop-store-webui-v2/src/scenes/Guest/scenes/HomePage/components/Slider/styles.ts
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    padding-bottom: 25px;
+    margin-bottom: 25px;
     *:focus {
         outline: 0;
     }
+    button {
+        padding: 40px 10px;
+    }
     svg {
+        height: unset !important;
         width: 30px !important;
     }
 `;
